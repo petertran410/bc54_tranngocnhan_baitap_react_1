4ec7ce7a2b7dd3194c7ab0995f1575b7b9b4245b
petertran410 <nhantran4102002@gmail.com>
2023-10-25 23:17:26 +0700

    1

---
+++ src/Header/header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default function Header() {
+  return (
+    <div>
+      <Header>
+        <div className="container">
+          <div className="title">
+            Start Bootstrap
+          </div>
+        </div>
+      </Header>
+    </div>
+  )
+}
